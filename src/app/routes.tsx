a05f7f9d42818848af30dcff929554e69b0c0a4a
Johnny Cage <thespammingbott@gmail.com>
2026-04-01 08:11:52 +0100

    feat: import all page components for route mapping

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,2 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+import { Browse } from "./pages/Browse";
+import { Library } from "./pages/Library";
+import { LikedSongs } from "./pages/LikedSongs";
