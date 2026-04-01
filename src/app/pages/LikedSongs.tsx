8e23cc4c87df9077563aba718e9257e065b9624e
Johnny Cage <thespammingbott@gmail.com>
2026-04-01 13:06:52 +0100

    feat: filter full track list down to only liked tracks using the Set

--- src/app/pages/LikedSongs.tsx
+++ src/app/pages/LikedSongs.tsx
@@ -3,3 +3,9 @@ import { TrackGrid } from '../components/TrackGrid';
 import { useMusicContext } from '../MusicContext';
 import { Heart } from 'lucide-react';
 
+export function LikedSongs() {
+  const { likedSongs } = useMusicContext();
+  const likedTracks = tracks.filter((track) => likedSongs.has(track.id));
+
+  
+}
