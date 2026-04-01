9c3cd0cf5479a7dd17a26cd56adef948b06a472d
Johnny Cage <thespammingbott@gmail.com>
2026-04-01 09:10:05 +0100

    import dependencies

---
+++ src/app/components/Layout.tsx
@@ -0,0 +1,7 @@
+import { Outlet } from 'react-router';
+import { Sidebar } from './Sidebar';
+import { BottomPlayer } from './BottomPlayer';
+import { useState } from 'react';
+import { Menu } from 'lucide-react';
+
+
