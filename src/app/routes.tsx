23cf0f37ff87d53f1b6844d8268f3d5a45b3ce1c
Johnny Cage <thespammingbott@gmail.com>
2026-04-01 08:12:58 +0100

    feat: create browser router with nested routes under Layout shell

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,3 +4,16 @@ import { Home } from "./pages/Home";
 import { Browse } from "./pages/Browse";
 import { Library } from "./pages/Library";
 import { LikedSongs } from "./pages/LikedSongs";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,         
+    children: [
+      { index: true, Component: Home },              
+      { path: "browse", Component: Browse },         
+      { path: "library", Component: Library },      
+      { path: "liked", Component: LikedSongs },  
+    ],
+  },
+]);
