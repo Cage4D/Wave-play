23ee1d10ae6141466d4cd401368f5b38078863da
Johnny Cage <thespammingbott@gmail.com>
2026-04-01 07:52:32 +0100

    feat: provide music context to all child routes and plug in browser router for SPA navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,11 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { MusicProvider } from './MusicContext';
+
+export default function App() {
+  return (
+    <MusicProvider>
+      <RouterProvider router={router} />
+    </MusicProvider>
+  );
+}
