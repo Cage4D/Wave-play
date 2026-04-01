e29639198e5854d03b2e5b51521e1e35ea1e6a62
Johnny Cage <thespammingbott@gmail.com>
2026-04-01 08:20:51 +0100

    feat: build BottomPlayer

---
+++ src/app/components/BottomPlayer.tsx
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { Play, Pause, SkipBack, SkipForward, Volume2, VolumeX, Heart } from 'lucide-react';
+import { MusicSlider } from './MusicSlider';
+import { ImageWithFallback } from './figma/ImageWithFallback';
